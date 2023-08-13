25287da4734d19c38d8a39b4a6fb714a839a4f29
HusrevAkbas <husrev.akbas@gmail.com>
2023-08-13 23:45:08 +0200

    first successful get request with user access token made. works fine. need to trim quotes from jwt token to store and to put in headers

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,16 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book } from '../models/book';
 import { Observable } from 'rxjs';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+    ) { }
+
+    // header:HttpHeaders = new HttpHeaders({ 'Access-Control-Allow-Origin': '*','content-type': 'application/json',"Authorization":"Bearer "+this.userService.jwttoken})
 
   getBooks(): Observable<any>{
-    return this.http.get("http://localhost:8080/api/book/all");
+    let key = 'Bearer '+ this.userService.jwttoken.replaceAll('"','');
+    let header2 = new HttpHeaders().set('Authorization', key)
+    console.log(key)
+    console.log(header2)
+    return this.http.get("http://localhost:8080/api/book/all",{headers:header2});
   }
 }
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { LoginRequest } from '../models/requests/login-request';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-signin',
@@ -9,7 +10,10 @@ import { LoginRequest } from '../models/requests/login-request';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-  constructor(private userService: UserService){}
+  constructor(
+    private userService: UserService,
+    private bookService: BookService
+    ){}
 
   responseData: any;
 
@@ -29,6 +33,16 @@ export class SigninComponent {
       this.responseData = response;
       console.log(this.userService.user)
     })
+  }
+
+  getBooks(){
+    this.bookService.getBooks().subscribe(res=>{
+      this.responseData=res
+      console.log(JSON.stringify(res))
+    });
+  }
+  log(){
+    console.log(this.userService.jwttoken)
   }
 
 }
